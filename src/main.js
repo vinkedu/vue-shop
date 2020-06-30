76db112526eaa8138489b6d02ce3e0cef735a9f4
dukaiwen <2848394217@qq.com>
2020-07-01 06:35:21 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
@@ -16,6 +18,7 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   store,
