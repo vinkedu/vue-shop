b5cf045ee0ded41758ca13993656952a2cc33ffb
dukaiwen <2848394217@qq.com>
2020-07-04 14:50:37 +0800

    完成了分类参数的开发

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Users from '../components/user/Users'
 import Rights from '../components/power/Rights'
 import Roles from '../components/power/Roles'
 import Cate from '../components/goods/Cate'
+import Params from '../components/goods/Params'
 
 Vue.use(VueRouter)
 
@@ -33,6 +34,9 @@ const routes = [
       },
       {
         path: '/categories', component: Cate
+      },
+      {
+        path: '/params', component: Params
       }
     ]
   },
